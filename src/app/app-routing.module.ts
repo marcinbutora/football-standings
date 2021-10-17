0f4d96f0a805e79107078d16f92ff0a7810fdd88
mb83pl <mbutora@gmail.com>
2021-10-17 09:57:49 +0200

    not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
 import { RankingPageComponent } from './pages/ranking/ranking-page/ranking-page.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'ranking', component: RankingPageComponent },
+  { path: '404', component: NotfoundComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: '**', redirectTo: '404' },
 ];
 
 @NgModule({
